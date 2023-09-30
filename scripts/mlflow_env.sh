a7ec41e4d7d2c8da024e879f4116c79fd4bae572
Abhishek-TyRnT <kulkarnia168@gmail.com>
2023-09-30 11:09:35 +0530

    added volumes to environment

--- scripts/mlflow_env.sh
+++ scripts/mlflow_env.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 HOME_DIR=$(dirname "$(dirname "$(readlink -f "$0")")")
-container_name=mlflow
+container_name=mlflow_container
 
 build()
 {
     #echo $HOME_DIR
-    docker build -t mlflow:latest -f $HOME_DIR/envs/Dockerfile --build-arg USERNAME=mlflow --build-arg UID=$(id -u) $HOME_DIR/envs
+    docker build \
+        -t mlflow:latest \
+        -f $HOME_DIR/envs/Dockerfile \
+        --build-arg USERNAME=mlflow \
+        --build-arg UID=$(id -u)\
+        --build-arg PASS=$1\
+         $HOME_DIR/envs
     docker volume create logs
+    docker volume create datasets
 }
 
 run()
@@ -20,10 +27,12 @@ run()
         docker start -i $container_name
     else
         docker run -it -v logs:/logs \
+            -v datasets:/datasets \
             -v $HOME_DIR:/home/mlflow/project \
+            -v $HOME/.kaggle:/home/mlflow/.kaggle \
+            --user $(id -u) \
             -w /home/mlflow/project \
-            --user $(id -u):$(id -g) \
-            --name mlflow \
+            --name $container_name \
             mlflow:latest
     fi
 
@@ -39,4 +48,4 @@ clean()
     fi
 }
 
-$1
+$1 $2
